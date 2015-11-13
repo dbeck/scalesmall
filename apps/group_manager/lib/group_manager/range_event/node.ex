0352262037feaec75e0ccd9a4cd3227d359c449b
David Beck <dbeck@starschema.net>
2015-11-13 23:39:14 +0100

    initial work on RangeEvent.Node

--- apps/group_manager/lib/group_manager/range_event/node.ex
+++ apps/group_manager/lib/group_manager/range_event/node.ex
@@ -3,8 +3,18 @@ defmodule GroupManager.RangeEvent.Node do
   TODO
   """
   
-  defstruct type: nil, node: nil, point: nil
-
+  @doc """
+  valid types are:
+  - :register
+  - :release
+  - :promote
+  - :demote
+  """
+  defstruct type: nil, node: nil, point: 1.0
   
+  def split(foreign_splits, own_events) when is_list(foreign_splits) and is_list(own_events) do
+    # TODO
+    own_events
+  end
   
 end
