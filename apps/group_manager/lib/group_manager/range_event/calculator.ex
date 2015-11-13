df424d158ca94109085a7f1f85cd16b3ac50e1dd
David Beck <dbeck@starschema.net>
2015-11-13 11:34:56 +0100

    scaffolding for calculators in status and range events

---
+++ apps/group_manager/lib/group_manager/range_event/calculator.ex
@@ -0,0 +1,5 @@
+defmodule GroupManager.RangeEvent.Calculator do
+  @moduledoc """
+  TODO
+  """
+end
---
+++ apps/group_manager/lib/group_manager/status_event/calculator.ex
@@ -0,0 +1,5 @@
+defmodule GroupManager.StatusEvent.Calculator do
+  @moduledoc """
+  TODO
+  """
+end
