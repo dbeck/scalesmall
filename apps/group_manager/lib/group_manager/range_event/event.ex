10e583f4ba9e54e907fb96c006dfce24b27ea7a2
David Beck <dbeck@starschema.net>
2015-11-13 15:33:45 +0100

    skeleton for merging range events

--- apps/group_manager/lib/group_manager/range_event/event.ex
+++ apps/group_manager/lib/group_manager/range_event/event.ex
@@ -2,4 +2,25 @@ defmodule GroupManager.RangeEvent.Event do
   @moduledoc """
   TODO
   """
+  
+  defstruct split: [], release: [], register: [], promote: [], demote: []
+
+  def merge(events) when is_list(events) do
+    List.foldl(events, %GroupManager.RangeEvent.Event{}, fn(x, acc) -> merge_two(x, acc) end)
+  end
+  
+  alias GroupManager.RangeEvent.Event, as: Event
+  
+  def merge_two(lhs, rhs) when is_map(lhs) and is_map(rhs) do
+
+    %Event{split: l_split, release: l_rel, register: l_reg, promote: l_prom, demote: l_dem} = lhs
+    %Event{split: r_split, release: r_rel, register: r_reg, promote: r_prom, demote: r_dem} = rhs
+    
+    # TODO
+    %Event{ split:    l_split ++ r_split,
+            release:  l_rel ++ r_rel,
+            register: l_reg ++ r_reg,
+            promote:  l_prom ++ r_prom,
+            demote:   l_dem ++ r_dem }
+  end
 end
--- apps/group_manager/lib/group_manager/range_event/node.ex
+++ apps/group_manager/lib/group_manager/range_event/node.ex
@@ -1,8 +1,9 @@
-defmodule GroupManager.RangeEvent.Demote do
+defmodule GroupManager.RangeEvent.Node do
   @moduledoc """
   TODO
   """
   
   defstruct type: nil, node: nil, point: nil
+  
   
 end
