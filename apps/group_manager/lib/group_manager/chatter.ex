7cdaca2ebb707d14552ae485672b9bb0f8dcb926
David Beck <dbeck@beckground.hu>
2016-02-07 00:03:18 +0100

    handle missing config variables

--- apps/group_manager/lib/group_manager/chatter.ex
+++ apps/group_manager/lib/group_manager/chatter.ex
@@ -29,9 +29,6 @@ defmodule GroupManager.Chatter do
 
   def init(:no_args)
   do
-    {:ok, mcast_ttl_str}  = Application.fetch_env(:group_manager, :multicast_ttl)
-    {multicast_ttl, ""}   = mcast_ttl_str  |> Integer.parse
-
     own_id    = local_netid()
     multi_id  = multicast_netid()
 
@@ -142,12 +139,40 @@ defmodule GroupManager.Chatter do
 
   def multicast_netid
   do
-    {:ok, mcast_addr_str}  = Application.fetch_env(:group_manager, :multicast_addr)
-    {:ok, mcast_port_str}  = Application.fetch_env(:group_manager, :multicast_port)
+    mcast_addr_str = case Application.fetch_env(:group_manager, :multicast_addr)
+    do
+      {:ok, val} ->
+        val
+      :error ->
+        Logger.info "no multicast_addr config value found for group_manager Application [default: 224.1.1.1]"
+        "224.1.1.1"
+    end
+
+    mcast_port_str = case Application.fetch_env(:group_manager, :multicast_port)
+    do
+      {:ok, val} ->
+        val
+      :error ->
+        Logger.info "no multicast_port config value found for group_manager Application [default: 29999]"
+        "29999"
+    end
 
     {:ok, multicast_addr} = mcast_addr_str |> String.to_char_list |> :inet_parse.address
     {multicast_port, ""}  = mcast_port_str |> Integer.parse
 
     NetID.new(multicast_addr, multicast_port)
+  end
+
+  def multicast_ttl
+  do
+    case Application.fetch_env(:group_manager, :multicast_ttl)
+    do
+      {:ok, mcast_ttl_str} ->
+        {multicast_ttl, ""}   = mcast_ttl_str  |> Integer.parse
+        multicast_ttl
+      :error ->
+        Logger.info "no multicast_ttl config value found for group_manager Application [default: 4]"
+        4
+    end
   end
 end
