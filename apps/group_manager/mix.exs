7fd1a2db8cebe84b3cc830f1033fcd2142760053
David Beck <dbeck@beckground.hu>
2016-02-18 22:22:32 +0100

    administer new version

--- apps/group_manager/mix.exs
+++ apps/group_manager/mix.exs
@@ -4,7 +4,7 @@ defmodule GroupManager.Mixfile do
   def project do
     [
       app: :group_manager,
-      version: "0.0.4",
+      version: "0.0.5",
       elixir: "~> 1.0",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Scalesmall.Mixfile do
 
   def project do
     [apps_path: "apps",
-     version: "0.0.4",
+     version: "0.0.5",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps]
